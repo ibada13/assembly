05bd7fdcbaaaf5238fef1831f2565e7c9e241d7e
hunter666hz <hzhunter7@gmail.com>
2024-01-13 14:19:54 +0100

    new file:   power.asm

---
+++ power.asm
@@ -0,0 +1,38 @@
+data segment 
+    val db 4
+    power db 3
+    msgp db "the number is even$"
+    msgi db "the number is odd$"
+ends
+
+
+
+
+code segment 
+    start:
+         mov ax,data
+         mov ds,ax
+         xor ax,ax
+         xor cl,cl
+         mov al,1
+         
+         mov bl,val
+         loop1:
+         
+         mul bl
+         
+         inc cl
+         cmp cl,power
+         jl loop1
+        mov ah,02h
+            mov dl,al
+            add dl,48
+            int 21h
+
+end:        
+  mov ah,4ch
+  int 21h  
+    
+    
+ends
+    end start
